ce56555e3eebd5290e88d5df6a67e6fab5bf425b
Melody Chung <melodc2@uci.edu>
2017-03-03 16:00:30 -0800

    Added Scene, Scripts Folder, and basic enemy movement script
    put your scenes in the scene folder and your scripts in the script
    folder

---
+++ ICS 161 Game 3/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Transform player1;
+    private Transform player2;
+
+    private NavMeshAgent nav;
+
+    private float dist1;
+    private float dist2;
+
+    void Awake()
+    {
+        player1 = GameObject.FindGameObjectWithTag("Player1").transform;
+        player2 = GameObject.FindGameObjectWithTag("Player2").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        dist1 = Vector3.Distance(transform.position, player1.position);
+        dist2 = Vector3.Distance(transform.position, player2.position);
+        print("dist1: " + dist1);
+        print("dist2: " + dist2);
+
+
+        if ( dist1 <= dist2)
+        {
+            nav.SetDestination(player1.position);
+        }
+        else
+        {
+            nav.SetDestination(player2.position);
+        }
+    }
+}
