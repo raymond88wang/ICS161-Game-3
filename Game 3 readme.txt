05ea1f58334857ac74ea1aa941d2e191a82b4940
Melody Chung <melodc2@uci.edu>
2017-03-03 17:01:44 -0800

    Added first person camera script

---
+++ Game 3 readme.txt
@@ -0,0 +1,2 @@
+[BORROWED CODE]
+SimpleSmoothMouseLook.cs | Lines 1-80 | https://forum.unity3d.com/threads/a-free-simple-smooth-mouselook.73117/
---
+++ ICS 161 Game 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public CharacterController c;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        c = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (c.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        c.Move(moveDirection * Time.deltaTime);
+    }
+}
