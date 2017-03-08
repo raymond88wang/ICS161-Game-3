536e6b54058623259abcf3186f5c33eebcb58da0
Melody Chung <melodc2@uci.edu>
2017-03-08 15:25:26 -0800

    added super basic item system
    - player can pick up item with "e" button
    - item is destroyed on pickup (might change this)
    - players can still pick up mutliple items, it's just when they pick up
    a new item the new item overwrites the old item

--- ICS 161 Game 3/Assets/Scripts/PlayerController.cs
+++ ICS 161 Game 3/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
@@ -7,15 +8,28 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     public CharacterController c;
+    public Text heldItem;
+
     private Vector3 moveDirection = Vector3.zero;
+    private bool canPickUp;
+    private string HeldItemName;
+    private GameObject itemToPickUp;
 
     void Start()
     {
         c = GetComponent<CharacterController>();
+        HeldItemName = "None";
+        UpdateHeldItemUI();
     }
 
     void Update()
     {
+        if(canPickUp && Input.GetKeyDown("e"))
+        {
+            HeldItemName = itemToPickUp.GetComponent<Item>().getName();
+            UpdateHeldItemUI();
+            Destroy(itemToPickUp);
+        }
 
         if (c.isGrounded)
         {
@@ -28,7 +42,35 @@ public class PlayerController : MonoBehaviour
         }
         moveDirection.y -= gravity * Time.deltaTime;
         c.Move(moveDirection * Time.deltaTime);
-        
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "item")
+        {
+            Debug.Log("Can pick up.");
+            canPickUp = true;
+            itemToPickUp = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "item")
+        {
+            Debug.Log("Cannot pick up.");
+            canPickUp = false;
+            itemToPickUp = null;
+        }
+    }
+
+    void UpdateHeldItemUI()
+    {
+        Debug.Log("currently held item: " + HeldItemName);
+    }
+
+
+
+
 
 }
