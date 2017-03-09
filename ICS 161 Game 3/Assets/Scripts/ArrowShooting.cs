1d90f1488887b2c4e872bed816998694c50114f1
aosterka <aosterka@uci.edu>
2017-03-08 18:56:39 -0800

    fuck
    my stupid ass shit

---
+++ ICS 161 Game 3/Assets/Scripts/ArrowShooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShooting : MonoBehaviour {
+
+    public GameObject prefabArrow;
+    public GameObject spawnPoint;
+    public Transform camDirection;
+    public float arrowForce;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKey("joystick 1 button 0"))  //can add xbox control in or statement
+        {
+            GameObject Temp_Arrow = Instantiate(prefabArrow, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+            Rigidbody Temp_rb = Temp_Arrow.GetComponent<Rigidbody>();
+            Temp_rb.AddForce(camDirection.transform.forward * arrowForce);
+
+            Destroy(Temp_Arrow, 7);
+        }
+
+
+
+    }
+}
