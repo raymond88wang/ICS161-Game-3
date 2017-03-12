9acf8d32da7e110a26549f540486b5749cb0c88c
aosterka <aosterka@uci.edu>
2017-03-12 11:08:26 -0700

    Platform Animation
    Added ability to lower platforms in main level

---
+++ Assets/Scripts/LowerPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerPlatform : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void LowerThePlatform()
+    {
+        anim.SetTrigger("Lower");
+    }
+}
