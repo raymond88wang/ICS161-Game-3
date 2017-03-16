725af224909b562937bd688104f70f0731b9aaf4
Raymond Wang <raymond88wang@gmail.com>
2017-03-16 01:54:30 -0700

    added box collider to stop force users to both arrive at boss and to close door behind

--- Assets/Scripts/BossFightStart.cs
+++ Assets/Scripts/BossFightStart.cs
@@ -3,14 +3,18 @@
 public class BossFightStart : MonoBehaviour {
 
     public GameObject bossHealthBar;
+    public Animator animator;
     private AudioSource bossBattle;
     private GameObject boss;
+    private GameObject player1;
+    private GameObject player2;
 
     private void Awake()
     {
         bossBattle = GetComponentInChildren<AudioSource>();
         boss = GameObject.Find("Boss");
         bossHealthBar.SetActive(false);
+        animator = transform.parent.GetComponentInChildren<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,8 +22,19 @@ public class BossFightStart : MonoBehaviour {
         GameObject.FindGameObjectWithTag("Game Music").GetComponent<AudioSource>().Stop();
         if(!bossBattle.isPlaying && boss != null && (other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2")))
         {
-            bossHealthBar.SetActive(true);
-            bossBattle.Play();
+            if(other.gameObject.CompareTag("Player1"))
+                player1 = other.gameObject;
+            if(other.gameObject.CompareTag("Player2"))
+                player2 = other.gameObject;
+
+            if(player1 != null && player2 != null)
+            {
+                bossHealthBar.SetActive(true);
+                bossBattle.Play();
+                animator.SetTrigger("Close");
+                DestroyObject(GetComponents<BoxCollider>()[1]);
+                DestroyObject(GetComponents<BoxCollider>()[0]);
+            }
         }
     }
 }
